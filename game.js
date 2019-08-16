e41f6a29cd74bdf5f512edc85682e209c0f5f569
Josiah Aubin <josiahaubin@gmail.com>
2019-08-16 15:48:29 -0600

    Buttons added and item func created, added mods

--- game.js
+++ game.js
@@ -5,26 +5,58 @@ let targetObj = {
   inventory: []
 }
 let items = {
-  damageAmplification: { name: "Zenyatta's Discord Orb", modifier: 2, description: "Damage is amplified" },
-  damageBoost: { name: "Ana's Nano Boost", modifier: 5, description: "Powered UP!" },
-  shield: { name: "Reinhardt's Barrier Field", modifier: 3, description: "Get behind my shield" }
+  nanoBoost: { name: "Ana's Nano Boost", modifier: -5, description: "Powered Up!!" },
+  repairPack: { name: "Brigitte's Repair Pack", modifier: 1, description: "Patching you up" },
+  shield: { name: "Reinhardt's Barrier Field", modifier: 5, description: "Get behind my shield" }
 }
 
+//This function lets the user fire D.Va's fusion cannons, dealing 1 damage to reaper
 function fusionCannons() {
-  targetObj.health--
+  targetObj.health -= 1 + addMods();
   targetObj.hits++
   update();
 }
+
+//This function lets the user fire D.Va's micro missiles, dealing 5 damage to reaper
 function microMissles() {
-  targetObj.health -= 5
+  targetObj.health -= 5 + addMods();
   targetObj.hits++
   update();
 }
+
+//This function lets the user perform D.Va's ultimate, dealing 10 damage to reaper
 function selfDestruct() {
-  targetObj.health -= 10
+  targetObj.health -= 10 + addMods();
   targetObj.hits++
   update();
 }
+
+//This function gives reaper damage reduction
+function giveShield() {
+  targetObj.inventory.push(items.shield);
+}
+
+//This function gives reaper damage amplification
+function giveNanoBoost() {
+  targetObj.inventory.push(items.nanoBoost);
+}
+
+//This function gives reaper damage reduction
+function giveRepairPack() {
+  targetObj.inventory.push(items.repairPack);
+}
+
+//This function adds up the total damage reduction for reaper
+function addMods() {
+  let totalModifier = 0;
+
+  for (let index = 0; index < targetObj.inventory.length; index++) {
+    totalModifier = totalModifier + targetObj.inventory[index].modifier;
+  }
+  return totalModifier;
+}
+
+//This function updates the screen
 function update() {
   let healthElem = document.getElementById('health');
   let targetElem = document.getElementById('targetName');
