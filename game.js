caabf0968e22a220acc7b407524f2980f94695be
Josiah Aubin <josiahaubin@gmail.com>
2019-08-16 13:40:47 -0600

    Target health on screen and updating

--- game.js
+++ game.js
@@ -2,4 +2,11 @@ let health = 100;
 
 function slap() {
   health = health - 1
+  update();
 }
+function update() {
+  let healthElem = document.getElementById('health');
+
+  healthElem.innerText = health.toString();
+}
+update();
