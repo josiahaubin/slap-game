696c1b0972a0faadedce9da3099708ba3bebb95a
Josiah Aubin <josiahaubin@gmail.com>
2019-08-16 14:07:39 -0600

    Added D.Va and extra buttons for attacks.

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
 let health = 100;
 
-function slap() {
+function fusionCannons() {
   health = health - 1
+  update();
+}
+function microMissles() {
+  health = health - 5
+  update();
+}
+function selfDestruct() {
+  health = health - 10
   update();
 }
 function update() {
